20333eead4c3673349aa065c4789eccff80ce38b
kpkq <tarasevich_a2000@mail.ru>
2020-09-22 16:26:26 +0300

    fixed getMemInfo

--- №2/main.cpp
+++ №2/main.cpp
@@ -2,23 +2,31 @@
 #include <Windows.h>
 #include <iostream>
 #include <string>
+#include <ntddscsi.h>
+
 using namespace std;
 
-void getMemInfo(_ULARGE_INTEGER& clusters, _ULARGE_INTEGER& freeClusters, _ULARGE_INTEGER& busyClusters)
+void getMemInfo(_ULARGE_INTEGER& clusters, _ULARGE_INTEGER& freeClusters, _ULARGE_INTEGER& busyClusters, int driveNumber)
 {
 	_ULARGE_INTEGER diskSpace = { 0 }, freeSpace = { 0 };
 	string mountPath("\\\\.\\ :"), volumePath(" :\\");
-	for (char volumeLetter = 'A'; volumeLetter <= 'D'; volumeLetter++)
+	for (char volumeLetter = 'A'; volumeLetter <= 'Z'; volumeLetter++)
 	{
 		mountPath[4] = volumeLetter;
 		HANDLE logicalDiskHandle = CreateFile(const_cast<char*>(mountPath.c_str()), GENERIC_READ,
 			FILE_SHARE_READ | FILE_SHARE_WRITE, NULL, OPEN_EXISTING, 0, NULL);
-		if (logicalDiskHandle == INVALID_HANDLE_VALUE)
-			continue;
+		if (logicalDiskHandle == INVALID_HANDLE_VALUE) continue;
+
 		volumePath[0] = volumeLetter;
+
+		STORAGE_DEVICE_NUMBER devNum = { 0 };
+		DeviceIoControl(logicalDiskHandle, IOCTL_STORAGE_GET_DEVICE_NUMBER, NULL, 0, &devNum, sizeof(devNum), NULL, NULL);
+		if (devNum.DeviceNumber != driveNumber) continue;
+
 		GetDiskFreeSpaceExA(const_cast<char*>(volumePath.c_str()), 0, &diskSpace, &freeSpace);
 		clusters.QuadPart += diskSpace.QuadPart;
 		freeClusters.QuadPart += freeSpace.QuadPart;
+		CloseHandle(logicalDiskHandle);
 	}
 	busyClusters.QuadPart = clusters.QuadPart - freeClusters.QuadPart;
 }
@@ -30,6 +38,10 @@ int main() {
 	_ULARGE_INTEGER freeClusters = { 0 }, clusters = { 0 }, busyClusters = { 0 };
 	hDevice = CreateFile(TEXT("\\\\.\\PhysicalDrive1"), NULL, FILE_SHARE_READ | FILE_SHARE_WRITE, NULL, OPEN_EXISTING, NULL, NULL);
 
+	STORAGE_DEVICE_NUMBER devNum = { 0 };
+
+	DeviceIoControl(hDevice, IOCTL_STORAGE_GET_DEVICE_NUMBER, NULL, 0, &devNum, sizeof(devNum), NULL, NULL);
+	unsigned long int logicalDrivesCount = GetLogicalDrives();
 	//==================================================================================================================================
 
 	STORAGE_PROPERTY_QUERY query;
@@ -60,13 +72,12 @@ int main() {
 		const char* interfaceTypes[] = { "Unknown", "SCSI", "ATAPI", "ATA", "1394", "SSA", "Fibre", "USB", "RAID", "ISCSI", "SAS", "SATA", "SD", "MMC", "Virtual", "FileBackedVirtual",
 		"Spaces", "Nvme", "SCM", "Ufs", "Max", "MaxReserved" };
 		printf("Interface type: %s\n", interfaceTypes[deviceDesc->BusType]);
-		getMemInfo(clusters, freeClusters, busyClusters);
+		getMemInfo(clusters, freeClusters, busyClusters, 1);
 		printf("Size = %lld bytes\nFree space = %lld bytes\nBusy space = %lld bytes\n", (long long int)clusters.QuadPart, freeClusters.QuadPart, busyClusters.QuadPart);
 	}
 	else {
 
 		cout << "Error: " << GetLastError() << endl;
 	}
-
 	return 0;
 }
