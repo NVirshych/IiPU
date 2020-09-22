7a02fc058101a50436aa2100cfdc811d52674be7
N.Virshych <n.virshych@yandex.by>
2020-09-22 16:00:13 +0300

    Refactor code

--- №2/main.cpp
+++ №2/main.cpp
@@ -1,73 +1,76 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <Windows.h>
 #include <iostream>
-#include <string>
 using namespace std;
 
-void getMemInfo(_ULARGE_INTEGER& clusters, _ULARGE_INTEGER& freeClusters, _ULARGE_INTEGER& busyClusters)
-{
+void getMemInfo(_ULARGE_INTEGER& clusters, _ULARGE_INTEGER& freeClusters, _ULARGE_INTEGER& busyClusters){
+
+	HANDLE logicalDiskHandle;
 	_ULARGE_INTEGER diskSpace = { 0 }, freeSpace = { 0 };
 	string mountPath("\\\\.\\ :"), volumePath(" :\\");
-	for (char volumeLetter = 'A'; volumeLetter <= 'D'; volumeLetter++)
-	{
+
+	for (char volumeLetter = 'A'; volumeLetter <= 'Z'; volumeLetter++){
+
 		mountPath[4] = volumeLetter;
-		HANDLE logicalDiskHandle = CreateFile(const_cast<char*>(mountPath.c_str()), GENERIC_READ,
-			FILE_SHARE_READ | FILE_SHARE_WRITE, NULL, OPEN_EXISTING, 0, NULL);
+		logicalDiskHandle = CreateFile(mountPath.c_str(), GENERIC_READ, FILE_SHARE_READ | FILE_SHARE_WRITE, NULL, OPEN_EXISTING, 0, NULL);
+		
 		if (logicalDiskHandle == INVALID_HANDLE_VALUE)
 			continue;
+
 		volumePath[0] = volumeLetter;
-		GetDiskFreeSpaceExA(const_cast<char*>(volumePath.c_str()), 0, &diskSpace, &freeSpace);
+		GetDiskFreeSpaceExA(volumePath.c_str(), 0, &diskSpace, &freeSpace);
 		clusters.QuadPart += diskSpace.QuadPart;
 		freeClusters.QuadPart += freeSpace.QuadPart;
+		CloseHandle(logicalDiskHandle);
 	}
 	busyClusters.QuadPart = clusters.QuadPart - freeClusters.QuadPart;
 }
 
 int main() {
-	HANDLE hDevice;
-	DWORD BytesReturned;
-	LPOVERLAPPED lpOverlapped = NULL;
-	_ULARGE_INTEGER freeClusters = { 0 }, clusters = { 0 }, busyClusters = { 0 };
-	hDevice = CreateFile(TEXT("\\\\.\\PhysicalDrive1"), NULL, FILE_SHARE_READ | FILE_SHARE_WRITE, NULL, OPEN_EXISTING, NULL, NULL);
 
-	//==================================================================================================================================
+	char data[400];
+	STORAGE_DEVICE_DESCRIPTOR* deviceDesc;
+	deviceDesc = (STORAGE_DEVICE_DESCRIPTOR*)&data;
+	_ULARGE_INTEGER freeClusters = { 0 }, clusters = { 0 }, busyClusters = { 0 };
+	const char* interfaceTypes[] = { "Unknown", "SCSI", "ATAPI", "ATA", "1394", "SSA", "Fibre", "USB", "RAID", "ISCSI", "SAS", "SATA", "SD", "MMC", "Virtual", "FileBackedVirtual",
+		"Spaces", "Nvme", "SCM", "Ufs", "Max", "MaxReserved" };
+
+	HANDLE hDevice;
+	hDevice = CreateFile(TEXT("\\\\.\\PhysicalDrive0"), NULL, FILE_SHARE_READ | FILE_SHARE_WRITE, NULL, OPEN_EXISTING, NULL, NULL);
+
+	DWORD dwIoControlCode = IOCTL_STORAGE_QUERY_PROPERTY;
 
 	STORAGE_PROPERTY_QUERY query;
 	ZeroMemory(&query, sizeof(query));
 	query.PropertyId = StorageDeviceProperty;
 	query.QueryType = PropertyStandardQuery;
-
-	char data[1024];
-
-	STORAGE_DEVICE_DESCRIPTOR* deviceDesc;
-	deviceDesc = (STORAGE_DEVICE_DESCRIPTOR*)&data;
-
-
-	DWORD dwIoControlCode = IOCTL_STORAGE_QUERY_PROPERTY;
 	LPVOID lpInBuffer = &query;
 	DWORD nInBufferSize = sizeof(query);
+
 	LPVOID lpOutBuffer = &data;
 	DWORD nOutBufferSize = sizeof(data);
 
+	DWORD BytesReturned;
+	LPOVERLAPPED lpOverlapped = NULL;
 
-	//                     OK                                                                                  OK              OK
 	if (DeviceIoControl(hDevice, dwIoControlCode, lpInBuffer, nInBufferSize, lpOutBuffer, nOutBufferSize, &BytesReturned, lpOverlapped)) {
 
 		printf("VendorID = %d\n", static_cast<int>(*data + deviceDesc->VendorIdOffset));
 		printf("Model = %s\n", data + deviceDesc->ProductIdOffset);
 		printf("Serial = %s\n", data + deviceDesc->SerialNumberOffset);
 		printf("Firmware revision = %s\n", data + deviceDesc->ProductRevisionOffset);
-
-		const char* interfaceTypes[] = { "Unknown", "SCSI", "ATAPI", "ATA", "1394", "SSA", "Fibre", "USB", "RAID", "ISCSI", "SAS", "SATA", "SD", "MMC", "Virtual", "FileBackedVirtual",
-		"Spaces", "Nvme", "SCM", "Ufs", "Max", "MaxReserved" };
 		printf("Interface type: %s\n", interfaceTypes[deviceDesc->BusType]);
-		getMemInfo(clusters, freeClusters, busyClusters);
-		printf("Size = %lld bytes\nFree space = %lld bytes\nBusy space = %lld bytes\n", (long long int)clusters.QuadPart, freeClusters.QuadPart, busyClusters.QuadPart);
 	}
 	else {
 	
 		cout << "Error: " << GetLastError() << endl;
 	}
 
+	getMemInfo(clusters, freeClusters, busyClusters);
+	printf("Size = %lld bytes\n", clusters.QuadPart);
+	printf("Free space = %lld bytes\n", freeClusters.QuadPart);
+	printf("Busy space = %lld bytes\n", busyClusters.QuadPart);
+
+	CloseHandle(hDevice);
 	return 0;
 }
